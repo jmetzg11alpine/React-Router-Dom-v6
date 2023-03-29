cb45f1b282520436020b2971858ee185d719f871
jmetzg11alpine <jmetzg11@gmail.com>
2023-03-29 11:45:14 -0500

    first and last

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,36 @@
+// NavLink can be styled
+
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <NavLink
+        to='/'
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/about'
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to='/products'
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to='/login'
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Login
+      </NavLink>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <section className='section'>
+      <h2>Donkey Home Page</h2>
+    </section>
+  )
+}
+export default Home
